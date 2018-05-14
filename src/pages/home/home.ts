e983e4681bc64258d47a368a8072fd33fa6efbac
jjsosa <jjsosa@clever-global.com>
2018-05-14 13:32:34 +0200

    Delete getUserFeed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,74 +1,6 @@
-// import { Component } from '@angular/core';
-// import { NavController } from 'ionic-angular';
-// import { Facebook } from '@ionic-native/facebook';
-//
-// @Component({
-//   selector: 'page-home',
-//   templateUrl: 'home.html'
-// })
-// export class HomePage {
-//
-//   isLoggedIn:boolean = false;
-//   users: any;
-//
-//   constructor(public navCtrl: NavController, private fb: Facebook) {
-//     fb.getLoginStatus()
-//       .then(res => {
-//         console.log(res.status);
-//         if(res.status === "connect") {
-//           this.isLoggedIn = true;
-//         } else {
-//           this.isLoggedIn = false;
-//         }
-//       })
-//       .catch(e => console.log(e));
-//   }
-//
-//   login() {
-//     this.fb.login(['public_profile', 'user_friends', 'email'])
-//       .then(res => {
-//         if(res.status === "connected") {
-//           this.isLoggedIn = true;
-//           this.getUserDetail(res.authResponse.userID);
-//         } else {
-//           this.isLoggedIn = false;
-//         }
-//       })
-//       .catch(e => console.log('Error logging into Facebook', e));
-//   }
-//
-//   logout() {
-//     this.fb.logout()
-//       .then( res => this.isLoggedIn = false)
-//       .catch(e => console.log('Error logout from Facebook', e));
-//   }
-//
-//   getUserDetail(userid) {
-//     this.fb.api("/"+userid+"/?fields=id,email,name,picture,gender",["public_profile"])
-//       .then(res => {
-//         console.log(res);
-//         this.users = res;
-//       })
-//       .catch(e => {
-//         console.log(e);
-//       });
-//   }
-//
-// }
-
-import {
-  Component
-}
-  from '@angular/core';
-import {
-  NavController
-}
-  from 'ionic-angular';
-import {
-  Facebook,
-  FacebookLoginResponse
-}
-  from '@ionic-native/facebook';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
@@ -86,13 +18,12 @@ export class HomePage {
   _lastName: String;
   _name: String;
   _data:any;
-  login() {
 
-    this.fb.login(['public_profile', 'user_friends', 'email','user_posts'])
+  login() {
+    this.fb.login(['public_profile', 'user_friends', 'email'])
       .then((res: FacebookLoginResponse) => {
         this.userId = res.authResponse.userID
         this.getUserInformation();
-        // this.getUserFeeds();
         this.navCtrl.push('MyprofilePage', {
           firstName: this._firstName,
           lastName: this._lastName,
@@ -100,7 +31,6 @@ export class HomePage {
           gender: this._gender,
           data:this._data
         })
-
       })
       .catch(e => console.log('Error logging into Facebook', e));
   }
@@ -112,20 +42,6 @@ export class HomePage {
           this._gender = JSON.parse(JSON.stringify(response)).gender;
           this._firstName = JSON.parse(JSON.stringify(response)).id;
           this._lastName = JSON.parse(JSON.stringify(response)).last_name;
-
-          this.getUserFeeds();
-        }, (error) => {
-          alert(error);
-        })
-      }
-    })
-  }
-  getUserFeeds() {
-    this.fb.getLoginStatus().then((res) => {
-      if (res.status == 'connected') {
-        this.fb.api('/me/feed', []).then((res) => {
-          this._data = res.data;
-          // alert(JSON.stringify(this._data));
         }, (error) => {
           alert(error);
         })
